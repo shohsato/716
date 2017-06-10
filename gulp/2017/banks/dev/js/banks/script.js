7a11fbdc5bd2d0b37e01b06615f03c2a662166f7
大里周史 <shohsato@gmail.com>
2017-06-10 17:31:59 +0900

    一応完成！

---
+++ gulp/2017/banks/dev/js/banks/script.js
@@ -0,0 +1,13 @@
+// flexslider
+$(window).load(function(){
+	$('.flexslider').flexslider({
+		animation: "slide",
+		animationLoop: true,
+		itemWidth: 340,
+		itemMargin: 16,
+		controlNav: false,
+		directionNav: false,
+	});
+});
+
+
